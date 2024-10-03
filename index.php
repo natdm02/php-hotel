d44a2e78fa588f7751aa752aff68ac300baefb38
natdm02 <natalidimarino2002@gmail.com>
2024-10-03 15:54:38 +0200

    [add]table

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-
+//arrey hotel
     $hotels = [
 
         [
@@ -40,12 +40,56 @@
 
     ];
 
-    foreach ($hotels as $hotel) {
-        echo "nome: " . $hotel['name'] . "<br>";
-        echo "descrizione: " . $hotel['description'] . "<br>";
-        echo "parcheggio: " . ($hotel['parking'] ? 'si' : 'no') . "<br>";
-        echo "voto: " . $hotel['vote'] . "<br>";
-        echo "distanza dal centro: " . $hotel['distance_to_center'] . " km<br><br>";
-    }
-    
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP HOTEL</title>
+    <!--BOOTSTRAP-->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+</head>
+
+<body>
+    <header>
+        H
+    </header>
+\
+    <main>
+    <div class="container my-5">
+        <div class="mb-4">
+            <h2>HOTEL</h2>
+        </div>
+        <table class="table table-bordered">
+            
+            <tr>
+                <th>nome</th>
+                <th>descrizione</th>
+                <th>parcheggio</th>
+                <th>voto</th>
+                <th>km</th>
+            </tr>
+
+            
+            <?php foreach ($hotels as $hotel) { ?>
+                <tr>
+                    <td><?php echo $hotel['name']; ?></td>
+                    <td><?php echo $hotel['description']; ?></td>
+                    <td><?php echo $hotel['parking'] ? 'Sì' : 'No'; ?></td>
+                    <td><?php echo $hotel['vote']; ?></td>
+                    <td><?php echo $hotel['distance_to_center']; ?> km</td>
+                </tr>
+            <?php } ?>
+        </table>
+    </div>
+
+    </main>
+    
+
+    <footer>
+
+    </footer>
+</body>
+</html>
+
