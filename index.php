f65f9e0c9dc1ec1a21e9b09d2b36aa74a9408902
natdm02 <natalidimarino2002@gmail.com>
2024-10-03 23:24:56 +0200

    [add] some css

--- index.php
+++ index.php
@@ -51,10 +51,12 @@
     <title>PHP HOTEL</title>
     <!--BOOTSTRAP-->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+    <!--css-->
+    <link rel="stylesheet" href="mine.css">
 </head>
 
 <body>
-    
+
 <main>
     <div class="container my-5">
         
